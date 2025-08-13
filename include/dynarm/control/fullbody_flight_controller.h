cd18e447334b62b3d28548029f2fdd991d3e33d1
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-08-13 15:47:37 +0900

    [WIP][control] workaround to implement flgith controller based on DDP and nonlinear control allocation

---
+++ include/dynarm/control/fullbody_flight_controller.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <pinocchio/fwd.hpp>
+#include <dynarm/model/manipulator_model.h>
+#include <dynarm/model/nonlinear_inverse_dynamics.h>
+#include <dynarm/control/ddp_hovering_problem.h>
+
+#include <aerial_robot_control/control/base/base.h>
+
+#include <ros/ros.h>
+#include <Eigen/Core>
+
+#include <sensor_msgs/JointState.h>
+#include <spinal/FourAxisCommand.h>
+
+namespace aerial_robot_control
+{
+class FullbodyFlightController : public ControlBase
+{
+public:
+  FullbodyFlightController();
+  virtual ~FullbodyFlightController() = default;
+
+  void virtual initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                          boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                          boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
+                          boost::shared_ptr<aerial_robot_navigation::BaseNavigator> navigator, double ctrl_loop_du);
+
+private:
+  ros::Publisher joints_control_pub_;
+  ros::Publisher gimbals_control_pub_;
+  ros::Publisher four_axis_command_pub_;
+
+  boost::shared_ptr<aerial_robot_model::ManipulatorRobotModel> dragon_arm_robot_model_;
+  std::shared_ptr<aerial_robot_dynamics::PinocchioRobotModel> pinocchio_robot_model_;
+  std::shared_ptr<pinocchio::Model> pinocchio_model_;
+  std::shared_ptr<pinocchio::Data> pinocchio_data_;
+
+  std::shared_ptr<DDPHoveringProblem> hovering_;
+  std::shared_ptr<crocoddyl::ShootingProblem> ddp_problem_;
+  std::shared_ptr<crocoddyl::SolverAbstract> ddp_solver_;
+  std::shared_ptr<aerial_robot_model::NonlinearInverseDynamics> nonlinear_inverse_dynamics_solver_;
+  std::vector<Eigen::VectorXd> xs_init_;
+  std::vector<Eigen::VectorXd> us_init_;
+
+  Eigen::VectorXd curr_target_q_;
+  Eigen::VectorXd curr_target_dq_;
+
+  Eigen::VectorXd control_input_;
+
+  void rosParamInit();
+  void DDPProblemInit();
+  virtual bool update() override;
+  virtual void reset() override;
+  void controlCore();
+  void sendCmd();
+  void sendFourAxisCommand();
+  void sendJointCommand();
+  void sendGimbalCommand();
+};
+}  // namespace aerial_robot_control
---
+++ src/control/fullbody_flight_controller.cpp
@@ -0,0 +1,287 @@
+#include <dynarm/control/fullbody_flight_controller.h>
+
+using namespace aerial_robot_control;
+
+FullbodyFlightController::FullbodyFlightController() : ControlBase()
+{
+}
+
+void FullbodyFlightController::initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
+                                          boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
+                                          boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator,
+                                          boost::shared_ptr<aerial_robot_navigation::BaseNavigator> navigator,
+                                          double ctrl_loop_du)
+{
+  ControlBase::initialize(nh, nhp, robot_model, estimator, navigator, ctrl_loop_du);
+
+  dragon_arm_robot_model_ = boost::dynamic_pointer_cast<aerial_robot_model::ManipulatorRobotModel>(robot_model);
+  pinocchio_robot_model_ = dragon_arm_robot_model_->getPinocchioRobotModel();
+  pinocchio_model_ = pinocchio_robot_model_->getModel();
+  pinocchio_data_ = pinocchio_robot_model_->getData();
+
+  four_axis_command_pub_ = nh_.advertise<spinal::FourAxisCommand>("four_axes/command", 1);
+  joints_control_pub_ = nh_.advertise<sensor_msgs::JointState>("joints_ctrl", 1);
+  gimbals_control_pub_ = nh_.advertise<sensor_msgs::JointState>("gimbals_ctrl", 1);
+
+  nonlinear_inverse_dynamics_solver_ =
+      std::make_shared<aerial_robot_model::NonlinearInverseDynamics>(nh, pinocchio_robot_model_);
+
+  rosParamInit();
+  DDPProblemInit();
+}
+
+void FullbodyFlightController::rosParamInit()
+{
+}
+
+void FullbodyFlightController::DDPProblemInit()
+{
+  std::vector<int> rotor_frame_indices = pinocchio_robot_model_->getRotorFrameIndices();
+  double m_f_rate = pinocchio_robot_model_->getMFRate();
+  std::vector<crocoddyl::Thruster> thrusters;
+  for (int i = 0; i < rotor_frame_indices.size(); i++)
+  {
+    int rotor_direction = pinocchio_robot_model_->getRotorDirection(i);
+    double thrust_lower_limit = pinocchio_robot_model_->getThrustLowerLimits()(i);
+    double thrust_upper_limit = pinocchio_robot_model_->getThrustUpperLimits()(i);
+    thrusters.emplace_back(rotor_frame_indices.at(i), (float)(abs(m_f_rate)),
+                           ((rotor_direction == 1) ? crocoddyl::ThrusterType::CCW : crocoddyl::ThrusterType::CW),
+                           (float)thrust_lower_limit, (float)thrust_upper_limit);
+  }
+
+  ros::NodeHandle ddp_nh(nh_, "ddp");
+  bool fwddyn;
+  {
+    ddp_nh.getParam("fwddyn", fwddyn);
+    std::cout << "fwddyn: " << fwddyn << std::endl;
+  }
+
+  DDPHoveringProblem::CostWeight cost_weight;
+  {
+    double whatever_double;
+    ddp_nh.getParam("state_weight", whatever_double);
+    cost_weight.state_weight = whatever_double;
+
+    ddp_nh.getParam("state_bound_weight", whatever_double);
+    cost_weight.state_bound_weight = whatever_double;
+
+    ddp_nh.getParam("control_weight", whatever_double);
+    cost_weight.control_weight = whatever_double;
+
+    std::vector<double> whatever_vector;
+    ddp_nh.getParam("x_state_weights", whatever_vector);
+    cost_weight.x_weights = Eigen::Map<Eigen::VectorXd>(whatever_vector.data(), whatever_vector.size());
+
+    if (fwddyn)
+      ddp_nh.getParam("u_weight_fwddyn", whatever_vector);
+    else
+      ddp_nh.getParam("u_weight_invdyn", whatever_vector);
+    cost_weight.u_weights = Eigen::Map<Eigen::VectorXd>(whatever_vector.data(), whatever_vector.size());
+
+    std::cout << "state_weight: " << cost_weight.state_weight << std::endl;
+    std::cout << "state_bound_weight: " << cost_weight.state_bound_weight << std::endl;
+    std::cout << "control_weight: " << cost_weight.control_weight << std::endl;
+    std::cout << "x_weights: " << cost_weight.x_weights.transpose() << std::endl;
+    std::cout << "u_weights: " << cost_weight.u_weights.transpose() << std::endl;
+  }
+  DDPHoveringProblem::OptimizationParam optimization_param;
+  std::cout << "horizon: " << optimization_param.horizon << std::endl;
+  std::cout << "dt: " << optimization_param.dt << std::endl;
+  std::cout << "max_iter: " << optimization_param.max_iter << std::endl;
+
+  hovering_ =
+      std::make_shared<DDPHoveringProblem>(pinocchio_model_, thrusters, fwddyn, cost_weight, optimization_param);
+}
+
+void FullbodyFlightController::reset()
+{
+  ControlBase::reset();
+
+  Eigen::VectorXd x0 = Eigen::VectorXd::Zero(pinocchio_model_->nq + pinocchio_model_->nv);
+  Eigen::VectorXd curr_q = dragon_arm_robot_model_->getCurrentJointPositions();
+  x0.head(pinocchio_model_->nq) = curr_q;  // includes root pose and joint positions
+  x0.head(3) << estimator_->getPos(Frame::BASELINK, estimate_mode_).x(),
+      estimator_->getPos(Frame::BASELINK, estimate_mode_).y(),
+      estimator_->getPos(Frame::BASELINK, estimate_mode_).z();  // root position
+  tf::Matrix3x3 root_rot = estimator_->getOrientation(Frame::BASELINK, estimate_mode_);
+  tf::Quaternion root_quat;
+  root_rot.getRotation(root_quat);
+  x0.segment(3, 4) << root_quat.x(), root_quat.y(), root_quat.z(), root_quat.w();  // root rotation
+  std::cout << "[ddp] x0: " << x0.transpose() << std::endl;
+
+  Eigen::VectorXd xref = Eigen::VectorXd::Zero(pinocchio_model_->nq + pinocchio_model_->nv);
+  xref.head(pinocchio_model_->nq) = curr_q;  // includes root pose and joint positions
+  xref.head(3) << estimator_->getPos(Frame::BASELINK, estimate_mode_).x(),
+      estimator_->getPos(Frame::BASELINK, estimate_mode_).y(), navigator_->getTargetPos().z();  // root position
+  xref.segment(3, 4) << root_quat.x(), root_quat.y(), root_quat.z(), root_quat.w();             // root rotation
+  std::cout << "[ddp] xref: " << xref.transpose() << std::endl;
+
+  curr_target_q_ = xref.head(pinocchio_model_->nq);
+
+  ddp_problem_ = hovering_->createHoveringProblem(x0, xref);
+  ddp_solver_ = std::make_shared<crocoddyl::SolverBoxFDDP>(ddp_problem_);
+
+  int N = hovering_->optimization_param_.horizon / hovering_->optimization_param_.dt;
+  xs_init_.resize(N, x0);
+  us_init_ = ddp_solver_->get_problem()->quasiStatic_xs(xs_init_);
+  xs_init_.push_back(x0);
+}
+
+bool FullbodyFlightController::update()
+{
+  if (!ControlBase::update())
+    return false;
+
+  controlCore();
+  sendCmd();
+
+  return true;
+}
+
+void FullbodyFlightController::controlCore()
+{
+  crocoddyl::Timer timer;
+  std::cout << "q0: " << ddp_problem_->get_x0().head(pinocchio_model_->nq).transpose() << std::endl;
+  std::cout << "v0: " << ddp_problem_->get_x0().tail(pinocchio_model_->nv).transpose() << std::endl;
+  ddp_solver_->solve(xs_init_, us_init_);
+  double time = timer.get_duration();
+  std::cout << "total calculation time: " << time << "[ms]" << std::endl;
+  std::cout << "Number of iterations: " << ddp_solver_->get_iter() << std::endl;
+  std::cout << "time per iterate: " << time / ddp_solver_->get_iter() << std::endl;
+  std::cout << "Total cost: " << ddp_solver_->get_cost() << std::endl;
+  std::cout << "Gradient norm: " << ddp_solver_->get_stop() << std::endl;
+
+  xs_init_ = ddp_solver_->get_xs();
+  us_init_ = ddp_solver_->get_us();
+
+  std::cout << "root ddq: " << us_init_.at(0).head(6).transpose() << std::endl;
+  std::cout << "joint ddq: " << us_init_.at(0).tail(us_init_.at(0).size() - 6).transpose() << std::endl;
+  std::cout << std::endl;
+  std::cout << "q final: " << xs_init_.back().head(pinocchio_model_->nq).transpose() << std::endl;
+  std::cout << "v final: " << xs_init_.back().tail(pinocchio_model_->nv).transpose() << std::endl;
+  std::cout << "root ddq final: " << us_init_.back().head(6).transpose() << std::endl;
+  std::cout << "joint ddq final: " << us_init_.back().tail(us_init_.back().size() - 6).transpose() << std::endl;
+  std::cout << std::endl;
+
+  // set current state as next initial state
+  Eigen::VectorXd current_x = Eigen::VectorXd::Zero(pinocchio_model_->nq + pinocchio_model_->nv);
+  current_x.head(pinocchio_model_->nq) = dragon_arm_robot_model_->getCurrentJointPositions();
+  current_x.head(3) << estimator_->getPos(Frame::BASELINK, estimate_mode_).x(),
+      estimator_->getPos(Frame::BASELINK, estimate_mode_).y(),
+      estimator_->getPos(Frame::BASELINK, estimate_mode_).z();  // root position
+
+  // root orientation
+  tf::Matrix3x3 root_rot = estimator_->getOrientation(Frame::BASELINK, estimate_mode_);
+  tf::Quaternion root_quat;
+  root_rot.getRotation(root_quat);
+  current_x.segment(3, 4) << root_quat.x(), root_quat.y(), root_quat.z(), root_quat.w();  // root rotation
+
+  // root linear velocity in local frame
+  tf::Vector3 root_vel = root_rot.inverse() * estimator_->getVel(Frame::BASELINK, estimate_mode_);
+  current_x.segment(pinocchio_model_->nq, 3) << root_vel.x(), root_vel.y(), root_vel.z();
+
+  // root angular velocity
+  current_x.segment(pinocchio_model_->nq + 3, 3) << estimator_->getAngularVel(Frame::BASELINK, estimate_mode_).x(),
+      estimator_->getAngularVel(Frame::BASELINK, estimate_mode_).y(),
+      estimator_->getAngularVel(Frame::BASELINK, estimate_mode_).z();
+
+  ddp_problem_->set_x0(current_x);
+
+  // initial guess in next loop
+  xs_init_.erase(xs_init_.begin());
+  xs_init_.push_back(xs_init_.back());
+  us_init_.erase(us_init_.begin());
+  us_init_.push_back(us_init_.back());
+
+  // solve inverse dynamics to get control input
+  Eigen::VectorXd target_ddq = us_init_.at(0);
+  Eigen::VectorXd control_input;
+  bool solved = nonlinear_inverse_dynamics_solver_->solve(
+      current_x.head(pinocchio_model_->nq), current_x.tail(pinocchio_model_->nv), target_ddq, control_input);
+  if (!solved)
+  {
+    ROS_ERROR("[ddp] Nonlinear inverse dynamics solver failed to solve.");
+    return;
+  }
+  std::cout << "generalized force: " << control_input.head(pinocchio_model_->nv).transpose() << std::endl;
+  std::cout << "thrust:"
+            << control_input.segment(pinocchio_model_->nv, pinocchio_robot_model_->getRotorNum()).transpose()
+            << std::endl;
+  std::cout << "gimbal angles: "
+            << control_input.tail(nonlinear_inverse_dynamics_solver_->getGimbalNames().size()).transpose() << std::endl;
+  control_input_ = control_input;
+  std::cout << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+}
+
+void FullbodyFlightController::sendCmd()
+{
+  sendFourAxisCommand();
+  sendJointCommand();
+  sendGimbalCommand();
+}
+
+void FullbodyFlightController::sendFourAxisCommand()
+{
+  Eigen::VectorXd thrust_upper_limits = pinocchio_robot_model_->getThrustUpperLimits();
+  Eigen::VectorXd thrust_lower_limits = pinocchio_robot_model_->getThrustLowerLimits();
+  spinal::FourAxisCommand four_axis_command_msg;
+  for (int i = 0; i < pinocchio_robot_model_->getRotorNum(); i++)
+  {
+    four_axis_command_msg.base_thrust.push_back(
+        std::min(std::max(thrust_lower_limits(i), control_input_(pinocchio_model_->nv + i)), thrust_upper_limits(i)));
+  }
+  four_axis_command_pub_.publish(four_axis_command_msg);
+}
+
+void FullbodyFlightController::sendJointCommand()
+{
+  sensor_msgs::JointState joint_state_msg;
+  joint_state_msg.header.stamp = ros::Time::now();
+
+  Eigen::VectorXd curr_target_q = curr_target_q_;
+  Eigen::VectorXd curr_target_dq = Eigen::VectorXd::Zero(pinocchio_model_->nv);
+  Eigen::VectorXd curr_target_tau = control_input_.segment(6, pinocchio_model_->nv - 6);  // exclude root ddq
+
+  std::vector<std::string> joint_names = nonlinear_inverse_dynamics_solver_->getJointNames();
+
+  for (int i = 0; i < joint_names.size(); i++)
+  {
+    if (joint_names.at(i).find("root") != std::string::npos)
+      continue;  // skip root joint
+
+    int joint_index_q = pinocchio_model_->joints[pinocchio_model_->getJointId(joint_names.at(i))].idx_q();
+    int joint_index_v = pinocchio_model_->joints[pinocchio_model_->getJointId(joint_names.at(i))].idx_v();
+
+    joint_state_msg.name.push_back(joint_names.at(i));
+    joint_state_msg.position.push_back(curr_target_q(joint_index_q));
+    joint_state_msg.velocity.push_back(curr_target_dq(joint_index_v));
+    joint_state_msg.effort.push_back(curr_target_tau(joint_index_v));
+  }
+  joints_control_pub_.publish(joint_state_msg);
+}
+
+void FullbodyFlightController::sendGimbalCommand()
+{
+  sensor_msgs::JointState gimbal_state_msg;
+  gimbal_state_msg.header.stamp = ros::Time::now();
+
+  std::vector<std::string> gimbal_names = nonlinear_inverse_dynamics_solver_->getGimbalNames();
+  Eigen::VectorXd target_gimbal_angles =
+      control_input_.tail(gimbal_names.size());  // Assuming gimbal angles are at the end of control_input_
+
+  for (int i = 0; i < gimbal_names.size(); i++)
+  {
+    double gimbal_angle = target_gimbal_angles(i);
+
+    gimbal_state_msg.name.push_back(gimbal_names.at(i));
+    gimbal_state_msg.position.push_back(gimbal_angle);
+  }
+
+  gimbals_control_pub_.publish(gimbal_state_msg);
+}
+
+/* plugin registration */
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(aerial_robot_control::FullbodyFlightController, aerial_robot_control::ControlBase);
