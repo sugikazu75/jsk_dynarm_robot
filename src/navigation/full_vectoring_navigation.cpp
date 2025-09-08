10762dd4138245924a91c8089ff70d48c71898e0
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-09-08 23:59:57 +0900

    [navigation] implement circle trajectory tracking for full vectoring navigation

--- src/navigation/full_vectoring_navigation.cpp
+++ src/navigation/full_vectoring_navigation.cpp
@@ -17,7 +17,49 @@ void FullVectoringNavigator::initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
   rosParamInit();
 
   desire_coordinate_sub_ =
-      nh_.subscribe("desire_coordinate", 1, &FullVectoringNavigator::desireCoordinatedCallback, this);
+      nh_.subscribe("desire_coordinate", 1, &FullVectoringNavigator::desireCoordinateCallback, this);
+  circle_trajectory_command_sub_ =
+      nh_.subscribe("circle_trajectory_command", 1, &FullVectoringNavigator::circleTrajectoryCommandCallback, this);
+}
+
+void FullVectoringNavigator::reset()
+{
+  BaseNavigator::reset();
+
+  circle_trajectory_flight_flag_ = false;
+}
+
+void FullVectoringNavigator::update()
+{
+  BaseNavigator::update();
+
+  circleTrajectoryGeneration();
+}
+
+void FullVectoringNavigator::circleTrajectoryGeneration()
+{
+  if (circle_trajectory_flight_flag_)
+  {
+    if (ros::Time::now().toSec() > circle_trajectory_end_time_)
+    {
+      Eigen::Vector3d target_pos = circle_center_ + Eigen::Vector3d(circle_radius_, 0.0, 0.0);
+      setTargetPosX(target_pos(0));
+      setTargetPosY(target_pos(1));
+      setTargetVel(0, 0, 0);
+      circle_trajectory_flight_flag_ = false;
+      ROS_INFO_STREAM("[navigation] finish circle trajectory tracking");
+    }
+    else if (ros::Time::now().toSec() >= circle_trajectory_start_time_)
+    {
+      double t = ros::Time::now().toSec() - circle_trajectory_start_time_;
+      Eigen::Vector3d target_pos = circle_center_ + Eigen::Vector3d(circle_radius_ * cos(circle_omega_ * t),
+                                                                    circle_radius_ * sin(circle_omega_ * t), 0.0);
+      setTargetVel(-circle_radius_ * circle_omega_ * sin(circle_omega_ * t),
+                   circle_radius_ * circle_omega_ * cos(circle_omega_ * t), 0);
+      setTargetPosX(target_pos(0));
+      setTargetPosY(target_pos(1));
+    }
+  }
 }
 
 void FullVectoringNavigator::desireCoordinateCallback(const spinal::DesireCoordConstPtr& msg)
@@ -25,6 +67,29 @@ void FullVectoringNavigator::desireCoordinateCallback(const spinal::DesireCoordC
   KDL::Rotation rot;
   rot = KDL::Rotation::RPY(msg->roll, msg->pitch, 0.0);
   robot_model_->setCogDesireOrientation(rot);
+}
+
+void FullVectoringNavigator::circleTrajectoryCommandCallback(const std_msgs::Float32MultiArrayConstPtr& msg)
+{
+  if (msg->data.size() != 2)
+  {
+    ROS_ERROR("[navigation] Circle trajectory command size mismatch.");
+  }
+  else
+  {
+    setTargetYaw(0.0);
+    circle_trajectory_flight_flag_ = true;
+    circle_radius_ = msg->data.at(0);
+    circle_omega_ = msg->data.at(1);
+    circle_trajectory_start_time_ = ros::Time::now().toSec() + 10.0;
+    circle_trajectory_end_time_ = circle_trajectory_start_time_ + 6.0 * M_PI / circle_omega_;  // three loops
+
+    tf::Vector3 target_pos = getTargetPos();
+    circle_center_ = Eigen::Vector3d(target_pos.x() - circle_radius_, target_pos.y(), target_pos.z());
+
+    ROS_INFO_STREAM("[navigation] circle trajectory center: " << circle_center_.transpose()
+                                                              << ", radius: " << circle_radius_);
+  }
 }
 
 /* plugin registration */
