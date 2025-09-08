8853d563a8fc653dc60c65470e33eb56f6c19ce5
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-09-08 23:59:57 +0900

    [navigation] implement circle trajectory tracking for full vectoring navigation

--- include/dynarm/navigation/full_vectoring_navigation.h
+++ include/dynarm/navigation/full_vectoring_navigation.h
@@ -3,6 +3,7 @@
 #include <aerial_robot_control/flight_navigation.h>
 #include <geometry_msgs/Vector3Stamped.h>
 #include <spinal/DesireCoord.h>
+#include <std_msgs/Float32MultiArray.h>
 
 namespace aerial_robot_navigation
 {
@@ -17,10 +18,25 @@ public:
   void initialize(ros::NodeHandle nh, ros::NodeHandle nhp,
                   boost::shared_ptr<aerial_robot_model::RobotModel> robot_model,
                   boost::shared_ptr<aerial_robot_estimation::StateEstimator> estimator, double loop_du) override;
+  void update() override;
+  void reset() override;
 
 private:
   ros::Subscriber desire_coordinate_sub_;
+  ros::Subscriber circle_trajectory_command_sub_;
+
+  // circle trajectory flight
+  bool circle_trajectory_flight_flag_ = false;
+  double circle_radius_;
+  double circle_omega_;
+  Eigen::Vector3d circle_center_;
+  double circle_trajectory_initial_yaw_;
+  double circle_trajectory_start_time_;
+  double circle_trajectory_end_time_;
+
+  void circleTrajectoryGeneration();
 
   void desireCoordinateCallback(const spinal::DesireCoordConstPtr& msg);
+  void circleTrajectoryCommandCallback(const std_msgs::Float32MultiArrayConstPtr& msg);
 };
 }  // namespace aerial_robot_navigation
