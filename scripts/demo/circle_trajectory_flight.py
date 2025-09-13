9d73262fbfb04d713294c47673772f4b6481c9d5
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-09-13 18:51:19 +0900

    [scripts] add demo sctipts for v2 model

---
+++ scripts/demo/circle_trajectory_flight.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+import rospy
+
+from aerial_robot_msgs.msg import FlightNav
+from geometry_msgs.msg import Vector3, Pose
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float32MultiArray
+import math
+import time
+
+
+class CircleTrajectoryFlightDemo:
+    def __init__(self):
+        self.circle_trajectory_pub = rospy.Publisher("circle_trajectory_command", Float32MultiArray, queue_size=1)
+        self.uav_nav_pub = rospy.Publisher("uav/nav", FlightNav, queue_size=1)
+        self.ddp_root_pose_pub = rospy.Publisher("root_pose_command", Pose, queue_size=1)
+        self.joint_command_pub = rospy.Publisher("joint_command", JointState, queue_size=1)
+        self.joints_control_pub = rospy.Publisher("joints_ctrl", JointState, queue_size=1)
+
+        self.radius = 0.5
+        self.angvel = 2.0
+
+        rospy.sleep(2.0)
+
+    def main(self):
+        rospy.loginfo("start main process")
+        rospy.loginfo("send initial joint angle")
+        joint_comnmand_msg = JointState()
+        joint_comnmand_msg.name = [
+            "joint1_yaw",
+            "joint1_pitch",
+            "joint2_yaw",
+            "joint2_pitch",
+            "joint3_yaw",
+            "joint3_pitch",
+        ]
+        joint_comnmand_msg.position = [math.pi / 2.0, 0.0, math.pi / 2.0, 0.0, math.pi / 2.0, 0.0]
+        self.joint_command_pub.publish(joint_comnmand_msg)
+        self.joints_control_pub.publish(joint_comnmand_msg)
+        time.sleep(5.0)
+
+        rospy.loginfo("move to start position")
+        uav_nav_msg = FlightNav()
+        uav_nav_msg.pos_xy_nav_mode = FlightNav.POS_MODE
+        uav_nav_msg.target_pos_x = 0.0
+        uav_nav_msg.target_pos_y = 0.0
+        uav_nav_msg.pos_z_nav_mode = FlightNav.POS_MODE
+        uav_nav_msg.target_pos_z = 1.0
+        uav_nav_msg.yaw_nav_mode = FlightNav.POS_MODE
+        uav_nav_msg.target_yaw = 0.0
+        self.uav_nav_pub.publish(uav_nav_msg)
+
+        root_pose_msg = Pose()
+        root_pose_msg.position.x = 0.0
+        root_pose_msg.position.y = 0.0
+        root_pose_msg.position.z = 1.0
+        root_pose_msg.orientation.x = 0.0
+        root_pose_msg.orientation.y = 0.0
+        root_pose_msg.orientation.z = 0.0
+        root_pose_msg.orientation.w = 1.0
+        self.ddp_root_pose_pub.publish(root_pose_msg)
+        time.sleep(10.0)
+
+        rospy.loginfo("send circle trajectory")
+        circle_trajectory_msg = Float32MultiArray()
+        circle_trajectory_msg.data.append(self.radius)
+        circle_trajectory_msg.data.append(self.angvel)
+        self.circle_trajectory_pub.publish(circle_trajectory_msg)
+
+
+if __name__ == "__main__":
+    rospy.init_node("circle_trajectory_flight_demo")
+    node = CircleTrajectoryFlightDemo()
+    node.main()
